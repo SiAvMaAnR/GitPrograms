27c892fc7fe828902dd230f8667f97a41b008242
SiAvMaAnR <samarkin200220022002@gmail.com>
2020-08-01 01:25:22 +0300

    Убрал слово Анкета

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -10,16 +11,17 @@ using Newtonsoft.Json.Linq;
 
 namespace GitPrograms
 {
+
 	public class Anketa
 	{
-		public string Name;//Имя
+		private string Name;//Имя
 		public string name
-		{ 
+		{
 			get
 			{
-				return name;
+				return Name;
 			}
-			set
+			private set
 			{
 				try
 				{
@@ -34,118 +36,124 @@ namespace GitPrograms
 			}
 		}
 
-		public string Surname { get; private set; }//Фамилия
-		public uint Age { get; private set; }//Возраст
-		public string Gender { get; private set; }//Пол
 
-		public DateTime dateTime { get; private set; }//Дата
-
-		Dictionary<string, object> _Anketa = new Dictionary<string, object>();//Словарь данных анкеты
-
-
-		//Функция обработки исключений
-		public void ProcessString(string name, string surname, uint age, string gender)
+		private string Surname;//Фамилия
+		public string surname
 		{
-			try
+			get 
 			{
-				//Проверка строк на null
-				if (name == null || surname == null || gender == null)
+				return Surname;
+			}
+			private set 
+			{
+				try
 				{
-					throw new ArgumentNullException();
+					if (value == null)
+						throw new ArgumentNullException();
 				}
-				//Проверка uint возраста на корректность
-				if (age < 0 || age > 200)
+				catch (ArgumentNullException)
 				{
-					throw new ArgumentOutOfRangeException();
+					Console.WriteLine("Исключение: Не корректная фамилия!");
 				}
-			}
-			//При наличии null строк вызов исключения
-			catch (ArgumentNullException)
-			{
-				Console.WriteLine("Исключение: Не корректные данные!");
-			}
-			//При наличии не корректного значения вызов исключения
-			catch (ArgumentOutOfRangeException)
-			{
-				Console.WriteLine("Исключение: Не корректный возраст!");
+				Surname = value;
 			}
 		}
 
 
-		//Передача аргументов в поля класса
-		public void Data_transfer(in string Name, in string Surname, in uint Age, in string Gender)
+		private uint Age;//Возраст
+		public uint age
 		{
-			//Передача аргементов в поля класса, при значении null вывод "none"
-			this.Name = Name??"none";
-			this.Surname = Surname ?? "none";
-			this.Age = Age;
-			this.Gender = Gender ?? "none";
-			dateTime = DateTime.Now;
+			get
+			{
+				return Age;
+			}
+			private set
+			{
+				try
+				{
+					if (value < 0)
+					{
+						throw new ArgumentOutOfRangeException();
+					}
+				}
+				catch (ArgumentOutOfRangeException)
+				{
+					Console.WriteLine("Исключение: Не корректный возраст!");
+				}
+				Age = value;
+			}
 		}
 
 
-		//Вывод полей класса
-		public void Print_info()
+		private string Gender;//Пол
+		public string gender
 		{
-			Console.WriteLine($"{Name} {Surname} {Age} {Gender} {dateTime}\n");
+			get
+			{
+				return Gender;
+			}
+			private set
+			{
+				try
+				{
+					if (value == null)
+					{
+						throw new ArgumentNullException();
+					}
+				}
+				catch (ArgumentNullException)
+				{
+					Console.WriteLine("Исключение: Не корректный пол!");
+				}
+				Gender = value;
+			}
 		}
+
+		public DateTime DateTime { get; private set; }//Дата
+
 
 
 		//Конструктор класса, с обработкой исключений и передачей аргументов в словарь
-		public Anketa(in string Name, in string Surname, in uint Age, in string Gender, DateTime dateTime=DateTime.Now)
+		public Anketa(in string Name, in string Surname, in uint Age, in string Gender)
 		{
-			//Метод вызова исключений
-			ProcessString(Name, Surname, Age, Gender);
-			//Метод передачи аргументов в поля класса
-			Data_transfer(Name, Surname, Age, Gender);
-
-			//Передача в словарь полей класса
-			_Anketa.Add("Name", this.Name);
-			_Anketa.Add("Surname", this.Surname);
-			_Anketa.Add("Age", this.Age);
-			_Anketa.Add("Gender", this.Gender);
-			_Anketa.Add("Data&Time", this.dateTime);
+			name = Name;
+			surname = Surname;
+			age = Age;
+			gender = Gender;
+			DateTime = DateTime.Now;
 		}
 
-
-		public Anketa(Dictionary<string, object> _Anketa_)
+		//Правописание «год», «года» или «лет»
+		private string Year(uint year)
 		{
-			
+			uint t1 = year % 10;
+			uint t2 = year % 100;
+			if (t1 == 1 && t2 != 11)
+				return "год";
+			if (t1 >= 2 && t1 <= 4 && (t2 < 10 || t2 >= 20))
+				return "года";
+			else
+				return "лет";
+		}
+		//Возврат строки
+		public string AnketaToString()
+		{
+			return $"{Name} {Surname} {Age} {Year(Age)} {Gender}";
 		}
 
-
-		//Сохранение сериализированных данных в JSON
-		public void Save()
-		{
-			File.WriteAllText("anketa.json", JsonConvert.SerializeObject(_Anketa));
-		}
-
-
-		//Чтение/Десериализация/Вывод JSON
-		public void Print_Person()
-		{
-			//Чтение, Десериализация и Запись JSON файла в словарь
-			Dictionary<string, object> _Anketa_ = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText( "anketa.json"));
-			//Вывод Словаря после записи данных из JSON
-			foreach (var item in _Anketa_)
-			{
-				Console.WriteLine(item.Key + " - " + item.Value);
-			}
-		}
 	};
 	class Program
 	{
 		//Имитация фронта
 		static void Main(string[] args)
 		{
-			string Name="Иван";
-			string Surname = null;
-			uint Age = 40;
+			string Name= "Иван";
+			string Surname = "Самаркин";
+			uint Age = 11561;
 			string Gender = "Оптимус Прайм";
 
 			Anketa anketa = new Anketa(Name, Surname, Age, Gender);
-			anketa.Save();
-			anketa.Print_Person();
+			Console.WriteLine(anketa.AnketaToString());
 		}
 	}
 }
